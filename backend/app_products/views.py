d1e2ee2b2179478e847247423125d4ce76dd75fd
sreenath k k <sreenathkk00@gmail.com>
2025-01-19 22:10:00 +0530

    Implemented backend API views for product creation, listing, deletion, and update

--- backend/app_products/views.py
+++ backend/app_products/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import CreateProductSerializer, ProductSerializer
+from .models import Products
 
-# Create your views here.
+class CreateProductAPIView(APIView):
+    def post(self, request):
+        serializer = CreateProductSerializer(data=request.data)
+        if serializer.is_valid():
+            product = serializer.save(context={'request': request})
+            return Response(ProductSerializer(product).data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class ListProductAPIView(APIView):
+    def get(self, request):
+        products = Products.objects.prefetch_related('variants__subvariants').all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+
+class ProductDeleteAPIView(APIView):
+    def delete(self, request, pk):
+        try:
+            product = Products.objects.get(pk=pk)
+            product.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except Products.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+
+class UpdateProductView(APIView):
+    def put(self, request, pk):
+        try:
+            product = Products.objects.get(pk=pk)
+        except Products.DoesNotExist:
+            return Response({"detail": "Product not found."}, status=status.HTTP_404_NOT_FOUND)
+
+        serializer = ProductSerializer(product, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
